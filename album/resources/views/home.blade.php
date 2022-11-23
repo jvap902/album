896473799af2ddb311ab42c3483d30ef9dee3687
Joao Pereira <joao.090405@bento.local>
2022-11-23 09:15:40 -0300

    estrutura album pronta

--- album/resources/views/home.blade.php
+++ album/resources/views/home.blade.php
@@ -1,26 +1,8 @@
 @extends('base.index')
 @section('container')
+    <div class="div">
 
-<nav class="nav justify-content-end">
-<a class="navbar-brand" href="#"></a>
-<div class="btn-group dropstart">
-  <a class="btn btn-outline-dark dropdown-toggle" class="btnmostrar" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-      <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-          <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
-          <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
-        </svg>
-  </a>
-
-  <ul class="dropdown-menu">
-    <li><a class="dropdown-item" href="#">Gerenciar figurinhas</a></li>
-    <li><a class="dropdown-item" href="#">Sair</a></li>
-  </ul>
-</div>
-</div>
-</nav>
-
-    <div>
-        <div class="div">
+        <div>
             <div class="div_album">
                 <div class="row">
                     <div class="col">
@@ -122,17 +104,46 @@
                 </div>
             </div>
             <div class="outros">
-                <div class="figurinhas_sweep">
-                    <div class="card">
+                <div class="nav">
+                    <nav class="nav justify-content-end">
+                        <a class="navbar-brand" href="#"></a>
+                        <div class="btn-group dropstart">
+                            <a class="btn btn-outline-dark dropdown-toggle" class="btnmostrar" href="#"
+                                role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25"
+                                    fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                                    <path fill-rule="evenodd"
+                                        d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+                                </svg>
+                            </a>
 
-                    </div>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Gerenciar figurinhas</a></li>
+                                <li><a class="dropdown-item" href="#">Sair</a></li>
+                            </ul>
+                        </div>
+                    </nav>
                 </div>
-                <div name="figurinhas">
-                    <form action="{{ route('login') }}" method='POST'>
-                        @csrf
+                <div class="pacotes_figurinhas">
+                    <div name="figurinhas" class="pacote">
+                        <form action="{{ route('login') }}" method='POST'>
+                            @csrf
 
-                        <a href="{{ route('createFigurinha') }}" class="btn">Ganhar Figurinha</a>
-                    </form>
+                            <a href="{{ route('createFigurinha') }}" class="btn">Ganhar Figurinha</a>
+                        </form>
+                    </div>
+                    <div class="figurinhas_sweep">
+                        <div class="card">
+
+                        </div>
+                    </div>
+                    <div class="repetidas">
+                        <div class="card">
+
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
+    </div>
