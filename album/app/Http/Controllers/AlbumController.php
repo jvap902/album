0442c4a397a6c3b8586a0b821eccfa084b82baf6
Alyssa Michalski <alyssa.070505@bento.local>
2022-12-07 08:51:48 -0300

    infoFigurinhas

--- album/app/Http/Controllers/AlbumController.php
+++ album/app/Http/Controllers/AlbumController.php
@@ -38,4 +38,34 @@ class AlbumController extends Controller
        
         return view('home', ['figurinhas' => $figurinhas, 'fuColadas' => $fuColadas, 'fuNaoColadas' => $fuNaoColadas]);
     }
+
+    function infoFigurinhas(request $request){
+        $uid = DB::table('usuarios')->where('email', $request->session()->get('usuario'))->value('id');
+
+        
+        $fuColadas = DB::table('figurinhas')->leftJoin('usuarios_figurinhas', 'figurinhas.id', '=', 'usuarios_figurinhas.figurinhas_id')->orderby('numero', 'DESC')->where([['usuarios_figurinhas.usuario_id', $uid], ['usuarios_figurinhas.colada', 1]])->get();
+
+
+        $figurinhas = DB::table('figurinhas')->orderby('numero', 'DESC')->get();
+
+        $colada = false; $n = 0;
+        foreach($figurinhas as $f){
+            foreach($fuColadas as $fc){
+                if($fc->figurinhas_id == $f->id){
+                    $colada = true;
+                    break;
+                }
+            }
+            if(!$colada){
+                $fuNaoColadas[$n] = $f;
+                $n++;
+            }
+            $colada = false;
+        }
+        foreach($fuColadas as $f){
+            $f->dtnasc = Carbon::parse($f->dtnasc)->format('d/m/Y');
+        }
+
+        return [$figurinhas, $fuColadas, $fuNaoColadas];
+    }
 }
