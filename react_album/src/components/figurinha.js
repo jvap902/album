80722c586d610512be905665a3a729c943b251e1
JRyan0 <104903761+JRyan0@users.noreply.github.com>
2022-12-13 14:13:53 -0300

    mudanças para o dnd e ajustes na rota

--- react_album/src/components/figurinha.js
+++ react_album/src/components/figurinha.js
@@ -1,28 +1,46 @@
 import React from "react";
+import { useDrop } from "react-dnd";
+import { TYPES } from "./types";
 
-export const Figurinha = ({ data }) => {
-  if (data.colada) {
-    return (
-      <div className="col">
-        <div className="card">
-          <img className="card-img-top fig_name" src={data.img} alt="Card cap" />
-          <div className="card-body">
-            <p className="card-text">{data.nome}</p>
-            <p className="card-text">{data.naturalidade}</p>
-            <p className="card-text">{data.dtnasc}</p>
-          </div>
-        </div>
-      </div>
-    );
-  } else {
-    return (
-      <div className="col">
-        <div className="card">
-          <div className="card-body n_colada">
-            <h2>{data.numero}</h2>
-          </div>
-        </div>
-      </div>
-    );
-  }
+export const Figurinha = ({ data, colaFigurinha, setFigAl }) => {
+    console.log(data)
+    const [{ backgroundColor }, dropRef] = useDrop(() => ({
+        accept: TYPES.FIGURINHA,
+        drop: (item) => {
+            colaFigurinha(item.id)
+            setFigAl(album => album.map(
+                p => p.numero === item.numero ? { ...p, colada: item } : p
+            ))
+        },
+        canDrop: (item) => item.numero === data.numero,
+        collect: (monitor) => ({
+            backgroundColor: monitor.isOver() ? 'black' : 'white'
+        })
+    }), [data.colada])
+
+
+    if (data.colada) {
+        return (
+            <div className="col" ref={dropRef}>
+                <div className="card">
+                    <img className="card-img-top fig_name" src={data.img} alt="Card cap" />
+                    <div className="card-body">
+                        <p className="card-text">{data.nome}</p>
+                        <p className="card-text">{data.naturalidade}</p>
+                        <p className="card-text">{data.dtnasc}</p>
+                    </div>
+                </div>
+            </div>
+        );
+    } else {
+        return (
+            <div className="col" ref={dropRef}>
+                <div className="card">
+                    <div className="card-body n_colada">
+                        <h2>{data.numero}</h2>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 };
