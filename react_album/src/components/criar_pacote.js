6ea0be2c84652d9c8ae4c27aaad97d13b8e3605a
Joao Pereira <joao.090405@bento.local>
2022-12-13 14:37:24 -0300

    a

--- react_album/src/components/criar_pacote.js
+++ react_album/src/components/criar_pacote.js
@@ -4,6 +4,7 @@ import { api } from "../api";
 export const CriarPacote = () => {
   const route_form = api.get("/login");
   const route_btn = api.get("/pacote/create");
+  console.log(route_btn)
   return (
     <div name="figurinhas" className="pacote">
       <form action={route_form} method="POST">
